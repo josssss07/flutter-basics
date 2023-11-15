44134dc5ba530072e50614e1ad8d00db1b33c087
josssss07 <joshuadesai3@gmail.com>
2023-11-15 17:25:42 +0530

    second app

---
+++ my_id_thingy/my_id/lib/main.dart
@@ -0,0 +1,95 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: JoshID(),
+  ));
+}
+
+class JoshID extends StatelessWidget {
+  const JoshID({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        // ignore: prefer_const_constructors
+        title: Text('Id card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 00),
+        // ignore: prefer_const_literals_to_create_immutables
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'NAME',
+                style: TextStyle(
+                  color: const Color.fromARGB(161, 255, 255, 255),
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Joshua Desai',
+                style: TextStyle(
+                  letterSpacing: 2.0,
+                  fontSize: 27.6,
+                  color: Colors.blueAccent,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                'Class',
+                style: TextStyle(
+                  color: const Color.fromARGB(161, 255, 255, 255),
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'SY Bsc IT',
+                style: TextStyle(
+                  letterSpacing: 2.0,
+                  fontSize: 27.6,
+                  color: Colors.blueAccent,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Text(
+                    'joshuadesai03@gmail.com',
+                    style: TextStyle(
+                      letterSpacing: 2.0,
+                      color: Colors.blueAccent,
+                    ),
+                  )
+                ],
+              ),
+            ]),
+      ),
+    );
+  }
+}
